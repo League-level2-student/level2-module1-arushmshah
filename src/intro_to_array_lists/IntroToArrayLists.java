0be8dc493fe181a5a38c708be0a5a73db5f5ed1a
League Student <league@iMac-16.attlocal.net>
2020-02-01 11:26:00 -0800

    intotoarraylsit - module 1

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,16 +1,28 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
+ArrayList<String> names = new ArrayList<String>();
+		
 		
 		//2. Add five Strings to your list
-		
+names.add("Jan");
+names.add("Sara");
+names.add("Fred");
+names.add("Marc");
+names.add("Jack");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = 0; i<names.size(); i++) {
+			System.out.println(names.get(i));
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for(String name: names ) {
+			System.out.println(name);
+		}
 		//5. Print only the even numbered elements in the list.
 		
 		//6. Print all the Strings in reverse order.
